7bae78358f8277974b9121517dae3fc334e99117
Matt Miller <mwm@fastmail.com>
2016-02-02 10:18:01 +0200

    Improve documentation.
    - Tighten up `convert` output spec from `number` to `float`
    - Reorganize docs for easier reading.. I think
    - Improve comment

--- lib/exoddic.ex
+++ lib/exoddic.ex
@@ -7,33 +7,15 @@ defmodule Exoddic do
     @typedoc """
     A keyword list with conversion options
 
-    - `from`: the supplied format
-    - `to`: the desired format
-    - `for_display`: whether to nicely format the output as a string
+    The `to` and `from` formats are identified by atoms corresponding to
+    the converter module names. They default to ':prob'
+
+    - `from`: the supplied input format
+    - `to`: the desired output format
+    - `for_display`: whether to nicely format the output as a string, defaults to true
 
     """
     @type exoddic_options :: [from: atom, to: atom, for_display: boolean]
-    @doc """
-    convert values among the various supported odds formats.
-
-    The `to` and `from` formats are identified by atoms corresponding to
-    the converter module names.
-
-    Conversion amounts provided as strings will receive a best effort attempt at conversion to
-    an appropriate number.
-
-    `for_display` defaults to true and will return an appropriately formatted string for the
-    requested item.  If set to false, it will return an unformatted number.
-    """
-    @spec convert(number | String.t, exoddic_options) :: String.t | number
-    def convert(amount, options \\ []) do
-      num_amount  = if is_bitstring(amount), do: destring(amount), else: amount
-      {from_module, to_module, for_display} = parse_options(options)
-
-      final_amount = num_amount |> from_module.to_prob |> to_module.from_prob
-
-      if for_display, do: to_module.for_display(final_amount), else: final_amount
-    end
 
     @spec parse_options(exoddic_options) :: {atom, atom, boolean}
     defp parse_options(options) do
@@ -48,12 +30,28 @@ defmodule Exoddic do
         Module.concat([__MODULE__, Converter, Keyword.get(options, which, :prob) |> Atom.to_string |> String.capitalize])
     end
 
+    @doc """
+    Convert values among the various supported odds formats.
+
+    Conversion amounts provided as strings will receive a best effort attempt at conversion to
+    an appropriate number.
+    """
+    @spec convert(number | String.t, exoddic_options) :: String.t | float
+    def convert(amount, options \\ []) do
+      num_amount  = if is_bitstring(amount), do: destring(amount), else: amount
+      {from_module, to_module, for_display} = parse_options(options)
+
+      final_amount = num_amount |> from_module.to_prob |> to_module.from_prob
+
+      if for_display, do: to_module.for_display(final_amount), else: final_amount
+    end
+
     @spec destring(String.t) :: float
     defp destring(maybe_num) do
       num = cond do
           Regex.match?(~r/^[0-9\/+-\.]+$/, maybe_num) -> Code.eval_string(maybe_num) |> (fn({x,_}) -> x end).()
           Regex.match?(~r/^[0-9\/\.]+%$/, maybe_num)  -> String.strip(maybe_num,?%) |> Float.parse |> (fn({x,_}) -> x/100 end).()
-          true                                        -> 0.0  # It's an error, but we're not doing anything about it at present
+          true                                        -> 0.0  # We don't handle strings we cannot easily understand as numbers
       end
       num / 1.0
     end
